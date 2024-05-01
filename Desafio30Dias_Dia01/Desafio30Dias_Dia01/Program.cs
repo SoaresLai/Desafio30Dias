f5284c350b111b03a2c058e4bd13434fab1fe9e8
Laiane Soares <lailaisoares1@gmail.com>
2024-05-01 15:53:22 -0300

    Primeiro dia

---
+++ Desafio30Dias_Dia01/Desafio30Dias_Dia01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Desafio30Dias_Dia01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o primeiro número: ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o segundo número: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            int sum = n1 + n2;
+
+            Console.WriteLine(" ");
+            Console.WriteLine("A soma dos valores é de: " + sum);
+        }
+    }
+}
