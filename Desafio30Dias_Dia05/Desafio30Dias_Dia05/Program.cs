0f332aa993f09576da904f4c897755f306d8b4b0
SoaresLai <161250700+SoaresLai@users.noreply.github.com>
2024-05-07 20:57:22 -0300

    Quinto dia

---
+++ Desafio30Dias_Dia05/Desafio30Dias_Dia05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Desafio30Dias_Dia05
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Insira um valor numérico inteiro: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n > 0 && n < 9)
+            {
+                Console.WriteLine("Dentro do valor permitido!");
+            }
+            else
+            {
+                Console.WriteLine("Fora do valor permitido!");
+            }
+        }
+    }
+}
