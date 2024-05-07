d53a8f5812696d22c397b36e17cd44173fe12b75
SoaresLai <161250700+SoaresLai@users.noreply.github.com>
2024-05-07 20:56:40 -0300

    Quarto dia

---
+++ Desafio30Dias_Dia04/Desafio30Dias_Dia04/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Desafio30Dias_Dia04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe os graus °C que deseja converter em °F: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int grau = (9 * n) / 5;
+
+            Console.WriteLine(n + " °C em °F é: " + grau);
+        }
+    }
+}
