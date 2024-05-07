041757f1b6064055b5b3330669f89f25c65d1e9b
SoaresLai <161250700+SoaresLai@users.noreply.github.com>
2024-05-07 20:58:53 -0300

    Sétimo dia

---
+++ Desafio30Dias_Dia07/Desafio30Dias_Dia07/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Desafio30Dias_Dia07
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int soma = 0;
+            
+            for (int i = 0; i <= 500; i++) 
+            {
+                soma += i;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
