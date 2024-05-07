04409d04334ab7c67913ee0f1cd6f185593c47f1
SoaresLai <161250700+SoaresLai@users.noreply.github.com>
2024-05-07 20:58:14 -0300

    Sexto dia

---
+++ Desafio30Dias_Dia06/Desafio30Dias_Dia06/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Desafio30Dias_Dia06
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string login = "dia06";
+            string senha = "a1b2c3";
+
+            Console.WriteLine("Insira o usuario: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("\nInsira a senha: ");
+            string pass = Console.ReadLine();
+
+            if (name == login &&  pass == senha)
+            {
+                Console.WriteLine("\nACESSO CONCEDIDO");
+            }
+            else
+            {
+                Console.WriteLine("\nACESSO NEGADO");
+            }
+        }
+    }
+}
